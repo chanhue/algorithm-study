4054c333f008411ea27ecc2d1e715657678b0927
chanhue <hwagchan@gmail.com>
2025-04-22 09:39:30 +0900

    [Edit] 2738

--- python/2738.py
+++ python/2738.py
@@ -1,9 +1,9 @@
 n, m = map(int, input().split())
 a = []
 b = []
-for i in range(m):
+for i in range(n):
     a.append(list(map(int, input().split())))
-for i in range(m):
+for i in range(n):
     b.append(list(map(int, input().split())))
 for i in range(n):
     for j in range(m):
